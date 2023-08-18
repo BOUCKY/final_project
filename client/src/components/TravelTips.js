b26f8f30137c88afbdc4488b17f171fdfc083dae
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-08-18 09:39:04 -0400

    Travel tips section in Travel101

--- client/src/components/TravelTips.js
+++ client/src/components/TravelTips.js
@@ -3,10 +3,42 @@ function TravelTips(){
         <div className='list-container'>
             <h1 className="list-header">The Ultimate Travel Tips</h1>
             <div className='tips-list'>
-                <li>hello</li>
-                <li>hello</li>
-                <li>hello</li>
-                <li>hello</li>
+                <li>Put black duct tape on any expensive electronics like camera equipment. If it looks broken, nobody wants to steal it.</li>
+                <li>Ear plugs. If you're traveling on a budget, you will probably be staying in some noisy located places. They really help.</li>
+                <li>Slip-on shoes...Comfort is key because you'll walk way more when traveling. These make it way easier to fly through security...And make sure you wear socks if you'll be going through an airport. You don't want to walk barefoot through security!</li>
+                <li>Always check the expiration date of your passport well before traveling. Many countries require six months of validity to grant entry.</li>
+                <li>Get a credit card that includes airport lounge access. Flying will never be the same.</li>
+                <li>Research your destination before you pack, so you know what you really need.</li>
+                <li>Pack a change of underwear, a toothbrush, medications, some cash, and a charger in whatever bag will be on your body the entire time you're traveling.</li>
+                <li>Plan ahead of time how you're going to get from the airport to your accommodation.</li>
+                <li>Carry a tote bag or laundry bag to separate your dirty clothes in your suitcase.</li>
+                <li>Pack an extra outfit in your carry-on, just in case.</li>
+                <li>Buy a diaper bag to pack photography equipment. They're cheap, waterproof, and nobody steals a diaper bag.</li>
+                <li>Never delete from your phone or throw away your boarding pass before you've actually entered your destination.</li>
+                <li>Scams are easily avoidable if you don't put yourself in vulnerable positions.</li>
+                <li>Instead of buying expensive international roaming and data from your cell provider, just buy a cheap prepaid rechargeable SIM card in whatever country you need service.</li>
+                <li>Street food can be amazing, but do your research about where it's safe to eat first.</li>
+                <li>Don't exchange money. Rather, use an ATM for the best exchange rate.</li>
+                <li>To avoid tangling, thread a straw with your delicate necklaces.</li>
+                <li>You only need to pack one power adapter and a powerboard. It's much cheaper than buying multiple adapters, and you end up with more sockets for all your electronics.</li>
+                <li>Keep copies of important travel documents on your phone and in the cloud so you can always access them.</li>
+                <li>Being flexible and ready to adapt is one of the most important parts of travelling.</li>
+                <li>The best time to book any holiday is off-season, not only in your own country but also in the place you're travelling to.</li>
+                <li>For the ladies: Research how the locals dress. If you look like you fit in and pay attention to your surroundings, no one will bother you.</li>
+                <li>Familiarize yourself with your destination ahead of time.</li>
+                <li>If you travel frequently, travel insurance is something you're going to want to get.</li>
+                <li>If your plans change and you need to cancel your hotel reservation against the hotel's cancellation policy, don't call and cancel. Instead, call the hotel and move your reservation to the following week — because many hotels will allow you to change a reservation without issue.</li>
+                <li>Keep a spreadsheet or list to stay organized.</li>
+                <li>Do the research and plan your meal times according to local customs. Many European restaurants close at 2 p.m. for a break and begin serving dinner later than when you might be used to in the States.</li>
+                <li>Rolled clothing takes up less space than folded clothing.</li>
+                <li>Download Maps.me. This app lets you download a map of the country you're visiting for free while you're using Wi-Fi, and later it can function as GPS and even recalculate without cell signal.</li>
+                <li>Pack your dirty shoes in a shower cap.</li>
+                <li>Thanksgiving can be a great time to travel internationally. Since it's an American holiday and people are traveling between states, ticket prices abroad go way down.</li>
+                <li>If you're traveling throughout Europe, buy an Interrail Pass. It's basically a continent-wide train ticket.</li>
+                <li>If you plan on sightseeing, try to walk from place to place. You'll learn much more about a city and the people by walking.</li>
+                <li>Hotel lobbies have great bathrooms. Find a luxury hotel and go to the bar, order an iced tea or a Coke, and then you can use a nice bathroom. Plus, most also have free Wi-Fi these days as well.</li>
+                <li>When it comes to booking accommodations, always read the reviews!</li>
+                <li>Always have cash on you.</li>
             </div>
         </div>
     )
